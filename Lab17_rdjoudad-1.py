ac2da175fd6a8753cba4dbbbb3bbf3778f36557c
Ryma Djoudad <zenyod85@gmail.com>
2026-05-15 17:26:29 -0400

    Book program

---
+++ Lab17_rdjoudad-1.py
@@ -0,0 +1,31 @@
+"""
+Hacker News API Code Refactor
+Ryma Djoudad
+To refactor Hacker News API's code
+Starter code from textbook
+05/15/2026
+"""
+
+from operator import itemgetter
+import requests
+
+# Make an API call and check the response.
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+# Process information about each submission.
+submission_ids = r.json()
+submission_dicts = []
+for submission_id in submission_ids[:30]:
+    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
+    r = requests.get(url)
+    print(f"id: {submission_id}\tstatus: {r.status_code}")
+    response_dict = r.json()
+
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link': f"https://news.ycombinator.com/item?id={submission_id}",
+        'comments': response_dict['descendants'],
+    }
+    submission_dicts.append(submission_dict)
